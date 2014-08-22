55d9f86c4218253008b2667787a351e20be09a98
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-08-22 14:45:54 +1200

    kinda works - a bit buggy tho

--- Custom-Transitions-01/DWPopAnimator.m
+++ Custom-Transitions-01/DWPopAnimator.m
@@ -10,4 +10,37 @@
 
 @implementation DWPopAnimator
 
+- (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    UIViewController* toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController* fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    
+    [[transitionContext containerView] addSubview:toViewController.view];
+    
+    
+    toViewController.view.frame =  CGRectMake(-fromViewController.view.bounds.size.width, 0,
+                                              fromViewController.view.bounds.size.width,
+                                              fromViewController.view.bounds.size.height);
+    
+    CGRect endFrame = CGRectMake(-fromViewController.view.bounds.size.width, 0,
+                                 fromViewController.view.bounds.size.width,
+                                 fromViewController.view.bounds.size.height);
+    
+    [UIView animateWithDuration:[self transitionDuration:transitionContext]
+                          delay:0
+         usingSpringWithDamping:1.0
+          initialSpringVelocity:0.5
+                        options:0
+                     animations:^{
+                         
+                         toViewController.view.frame = fromViewController.view.frame;
+//                         fromViewController.view.frame = endFrame;
+                         
+                         
+                     } completion:^(BOOL finished) {
+                         [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
+                         NSLog(@"POP - %d view controllers present", toViewController.navigationController.viewControllers.count);
+                     }];
+}
+
 @end
--- Custom-Transitions-01/DWPushAnimator.m
+++ Custom-Transitions-01/DWPushAnimator.m
@@ -15,10 +15,8 @@
     UIViewController* toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     UIViewController* fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     
-//    [[transitionContext containerView] addSubview:fromViewController.view];
     [[transitionContext containerView] addSubview:toViewController.view];
-    
-//    toViewController.view.alpha = 0;
+
     
     toViewController.view.frame =  CGRectMake(fromViewController.view.bounds.size.width, 0,
                                              fromViewController.view.bounds.size.width,
@@ -29,50 +27,20 @@
                                  fromViewController.view.bounds.size.height);
     
     [UIView animateWithDuration:[self transitionDuration:transitionContext]
-                          delay:0 usingSpringWithDamping:1.0
+                          delay:0
+         usingSpringWithDamping:1.0
           initialSpringVelocity:0.5
                         options:0
                      animations:^{
-//                         fromViewController.view.transform = CGAffineTransformMakeScale(0.1, 0.1);
-//                         fromViewController.view.transform = CGAffineTransformMakeTranslation(fromViewController.view.bounds.size.width, 0.0f);
-//                         toViewController.view.alpha = 1;
                          
                          toViewController.view.frame = fromViewController.view.frame;
                          fromViewController.view.frame = endFrame;
                          
                          
                      } completion:^(BOOL finished) {
-                         fromViewController.view.transform = CGAffineTransformIdentity;
                          [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
+                          NSLog(@"PUSH - %d view controllers present", toViewController.navigationController.viewControllers.count);
                      }];
 }
 
-
-
-/*
- 
- newC.view.frame =  CGRectMake(self.view.bounds.size.width, 0,
- self.view.bounds.size.width,
- self.view.bounds.size.height);
- 
- CGRect endFrame = CGRectMake(-self.view.bounds.size.width, 0,
- self.view.bounds.size.width,
- self.view.bounds.size.height)
- 
- [self transitionFromViewController: oldC
- toViewController: newC
- duration: 0.15
- options: UIViewAnimationOptionCurveEaseOut
- animations:
- ^{
- newC.view.frame = oldC.view.frame;
- oldC.view.frame = endFrame;
- }
- completion:^(BOOL finished) {
- [oldC removeFromParentViewController];
- [newC didMoveToParentViewController:self];
- }];
- 
- 
- */
 @end
